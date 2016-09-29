d9fbcaa640b3f62528a2b037c317e3638a7bc4be
Hank <hankhe@gmail.com>
2016-09-29 18:32:41 +0800

    build_relationship_between_comment_and_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :topics, dependent: :destroy     
-         
+    has_many :comments, dependent: :destroy     
    	def short_name
    		self.email.split("@").first
    	end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929100326) do
+ActiveRecord::Schema.define(version: 20160929102435) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
     t.index ["topic_id"], name: "index_comments_on_topic_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "topics", force: :cascade do |t|
