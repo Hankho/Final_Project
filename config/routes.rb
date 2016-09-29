4bfbf027ea4c3222231bab9285a02017af4a1aef
Hank <hankhe@gmail.com>
2016-09-29 17:20:20 +0800

    add_devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
 	resources :topics do
-		resources :comments
+		resources :comments, :controller => "topic_comments"
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
