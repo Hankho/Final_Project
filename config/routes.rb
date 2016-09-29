8b2852a2443dfc8936efb03c22cb0bdc44c5ad32
Hank <hankhe@gmail.com>
2016-09-29 18:20:38 +0800

    build_relationship_between_topic_and_user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 	resources :topics do
 		resources :comments, :controller => "topic_comments"
 	end
+	resources :users do
+		resources :topics 
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
